d827d40ad26e90a2389d34952da718a1183d3aed
Germanium-Morina <portfolio.germanium@gmail.com>
2024-07-25 15:40:15 +0200

    styling register

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,38 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Register from './Components/Auth/Register';
+import Login from './Components/Auth/Login'
+import List from './Components/Table/List'
+import Logout from './Components/Auth/Logout'
+import { MDBIcon } from 'mdb-react-ui-kit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="flex h-screen w-full flex-col bg-gray-200 pr-7 pl-7 text-gray-500">
+
+        <nav className='flex items-end justify-end p-3 text-center pl-6 pr-6 bg-white  mt-3 rounded-lg'>
+          <ul className='flex justify-between items-center h-full p-0 m-0 w-full'>
+            <li className='flex justify-center items-center'>
+              <h1 className='p-0 m-0 flex items-center justify-center text-xl font-bold'>
+                TO-DO LIST
+              </h1>
+            </li>
+            <li><Link to="/logout" className='text-gray-500 flex flex-row gap-2 justify-center items-center'>
+              <MDBIcon fas icon="sign-out-alt" />
+              Logout</Link></li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/list" element={<List />} />
+        </Routes>
+
+      </div>
+    </Router>
   );
 }
 
--- front-end/src/Components/Auth/Register.js
+++ front-end/src/Components/Auth/Register.js
@@ -1,7 +1,110 @@
 import React from 'react'
+import { useState } from 'react'
+import { MDBIcon } from 'mdb-react-ui-kit';
+function Register() {
+  const [submitted, setSubmitted] = useState(false);
+  const [form, setForm] = useState({
+    fullName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  })
+  const [errors, setErrors] = useState({});
 
-export default function Register() {
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setForm({ ...form, [name]: value })
+  }
+
+  const validateForm = () => {
+    const errors = {}
+    if (!form.fullName) {
+      errors.fullName = 'Full Name is required'
+    }
+    if (!form.email) {
+      errors.email = 'Email is required'
+    } else if (!/^\S+@\S+\.\S+$/.test(form.email)) {
+      errors.email = 'Invalid email format'
+    }
+    if (!form.password) {
+      errors.password = 'Password is required'
+    } else if (form.password.length < 6) {
+      errors.password = 'Password must be at least 8 characters long'
+    }
+    if (!form.confirmPassword) {
+      errors.confirmPassword = 'Confirm Password is required'
+    } else if (form.confirmPassword !== form.password) {
+      errors.confirmPassword = 'Passwords do not match'
+    }
+    setErrors(errors)
+    return Object.keys(errors).length === 0
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setSubmitted(true)
+    if (validateForm()) {
+      // API call here to register user
+    }
+  }
+
+
   return (
-    <div>Register</div>
+    <div className='flex justify-center items-center w-full h-full'>
+      <div className='flex flex-col bg-white pl-6 pr-6 pt-3 rounded-lg'>
+        <div className='flex justify-start items-center gap-2 mb-4'>
+          <MDBIcon fas icon="user-plus" size='lg'/>
+          <h1 className='text-xl m-0'>
+            Register
+          </h1>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Full Name</label>
+            <input
+              type="text"
+              name="fullName"
+              value={form.fullName}
+              onChange={handleChange}
+            />
+            {errors.fullName && <span>{errors.fullName}</span>}
+          </div>
+          <div>
+            <label>Email</label>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+            />
+            {errors.email && <span>{errors.email}</span>}
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              value={form.password}
+              onChange={handleChange}
+            />
+            {errors.password && <span>{errors.password}</span>}
+          </div>
+          <div>
+            <label>Confirm Password</label>
+
+            <input
+              type="password"
+              name="confirmPassword"
+              value={form.confirmPassword}
+              onChange={handleChange}
+            />
+            {errors.confirmPassword && <span>{errors.confirmPassword}</span>}
+            <button type="submit">Register</button>
+          </div>
+        </form>
+      </div>
+
+    </div>
   )
 }
+export default Register
